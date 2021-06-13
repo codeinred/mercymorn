76164e3de2b3f0e6a5b013db9b4e98d1b0279b09
Alecto Perez <perez.cs@pm.me>
2021-06-13 17:46:30 -0600

    Added playfair font

--- getfonts.sh
+++ getfonts.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-mkdir -p assets/fonts
+mkdir -p static/fonts
 
-cd assets/fonts
-for font in "comfortaa" "ibm-plex-serif" "source-code-pro"
+cd static/fonts
+
+for font in "comfortaa" "ibm-plex-serif" "source-code-pro" "playfair-display"
 do
     curl -o $font.zip \
         "https://google-webfonts-helper.herokuapp.com/api/fonts/$font?download=zip&subsets=latin&formats=woff,woff2&variants=regular,italic,700,700italic"
-    unzip $font.zip
+    unzip -o $font.zip
     rm $font.zip
 done
