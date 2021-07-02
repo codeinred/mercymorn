ba835ff0a5f86233d2349960a09a55b3107c148d
Alecto Perez <perez.cs@pm.me>
2021-07-02 01:38:40 -0600

    Added fira-code

--- getfonts.sh
+++ getfonts.sh
@@ -4,7 +4,7 @@ mkdir -p static/fonts
 
 cd static/fonts
 
-for font in "comfortaa" "ibm-plex-serif" "source-code-pro" "playfair-display"
+for font in "fira-code" "comfortaa" "ibm-plex-serif" "source-code-pro" "playfair-display"
 do
     curl -o $font.zip \
         "https://google-webfonts-helper.herokuapp.com/api/fonts/$font?download=zip&subsets=latin&formats=woff,woff2&variants=regular,italic,700,700italic"
